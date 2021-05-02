dd027993a9ff7c4dff9547ef2d7efcd082de9510
RenatoSerk <brickworked@gmail.com>
2021-05-02 15:57:20 -0400

    Initial searchBar commit
    Init

---
+++ src/searchBar/SearchBar.tsx
@@ -0,0 +1,10 @@
+import React, {Component, ReactElement} from 'react';
+
+interface searchProps {}
+
+export default class Dropzone extends Component<searchProps>{
+
+    render() : ReactElement {
+        return (<div>Inner city pressure</div>);
+    }
+}
